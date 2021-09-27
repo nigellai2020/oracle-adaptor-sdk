4ec2ebb2949af775bf3bf3a181b021b597fdc537
benho-ijs <benho@instajs.com>
2021-09-27 14:00:36 +0800

    update test

--- test/helper.ts
+++ test/helper.ts
@@ -2,6 +2,8 @@ import { ethers, waffle } from "hardhat";
 import {BigNumber, BigNumberish, Contract, ContractFactory} from "ethers";
 import {expect, use} from "chai";
 
+const provider = waffle.provider;
+
 export function toWei(value: string, unit?: any): BigNumber {
     return ethers.utils.parseUnits(value, unit);
 }
@@ -12,33 +14,48 @@ export function toWeiInv(n: string, unit?: any) {
 export function fromWei(v: BigNumberish): string {
     return ethers.utils.formatEther(v);
 }
+export function setTime(time: number){
+    return provider.send(time > 1000000000 ? 'evm_mine' : 'evm_increaseTime',[time]);
+}
 export function compare(o1: any, o2: any) {
     for (let k in o2) {
-        if (Array.isArray(o1[k]))
-            expect(o1[k]).to.deep.equal(o2[k]);
-        else
-            expect(o1[k]).to.equal(o2[k]);
+        if (Array.isArray(o1[k])) {
+            for (let i = 0 ; i < o1[k].length ; i++)
+                compare(o1[k], o2[k]);
+            // expect(o1[k]).to.deep.equal(o2[k]);
+        } else
+            expect(o1[k], k).to.equal(o2[k]);
     }
 }
-export function print(...args: any[]): void {
-    for (let i = 0 ; i < args.length ; i++) {
-        _print(args[i]);
-    }
-}
-function _print(o:any): void {
-    if (o._isBigNumber) {
-        console.log(o.gt(10000000)?fromWei(o):o.toString());
+export function print(o: any, indent: string = "") {
+    if (!o) {
+        console.log(indent, o);
+    } else if (BigNumber.isBigNumber(o)) {
+        console.log(indent, o.gt("2000000000")?fromWei(o.toString()):o.toString());
+    } else if (typeof o === "string") {
+        console.log(indent, /\d{9,}/.test(o)?fromWei(o):o);
+    } else if (typeof o === "number") {
+        console.log(indent, o>2000000000?fromWei(o):o);
+    } else if (typeof o === "boolean") {
+        console.log(indent, o);
     } else if (typeof o === "object") {
-        for (let k in o) {
-            if (o[k]._isBigNumber) {
-                console.log(k, o[k].gt(10000000)?fromWei(o[k]):o[k].toString());
+         for (let k in o) {
+            if (!o[k]){
+                console.log(indent, k, ":", o[k]);
+            } else if (BigNumber.isBigNumber(o[k])) {
+                console.log(indent, k, ":", o[k].gt("2000000000")?fromWei(o[k].toString()):o[k].toString());
             } else if (Array.isArray(o[k])) {
-                console.log(k, o[k].map((e:any) => e._isBigNumber ? (e.gt(1000000)?fromWei(e):e.toString()) : e));
+                console.log(indent, k, ":", o[k].map((e:any) => BigNumber.isBigNumber(e) ? (e.gt("2000000000")?fromWei(e):e.toString()) : e));
+            } else if (typeof o[k] === "number" || typeof o[k] === "string" || typeof o[k] === "boolean") {
+                console.log(indent, k, ":", o[k]); 
             } else {
-                console.log(k, o[k]);
+                //console.log(k, o[k]);
+                console.log(indent, k, ": {");
+                print(o[k], indent+"  ");
+                console.log(indent, "}");
             }
         }
     } else {
-        console.log(o);
+        console.log(indent, o);
     }
 }
